2438125c50b5a585bc991dfe634c8aa6a10ee414
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-20 08:49:12 -0500

    Partial pseudo code, readme written, problem linked

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+/**
+ * Created by IAN on 20/11/14.
+ */
+public class Main {
+    public static void main(String args[]){
+
+        /**
+         *
+         * Linear representation of 2D array
+         *
+         *  0  1  2  3  4
+         *  5  6  7  8  9
+         *  10 11 12 13 14
+         *  15 16 17 18 19
+         *  20 21 22 23 24
+         *
+         * Set of all possible dominos == 6 x 6 == 36
+         *
+         * 1 1, 1 2, 1 3, 1 4, 1 5, 1 6
+         * 2 1, 2 2, 2 3, 2 4, 2 5, 2 6
+         * 3 1, 3 2, 3 3, 3 4, 3 5, 3 6
+         * 4 1, 4 2, 4 3, 4 4, 4 5, 4 6
+         * 5 1, 5 2, 5 3, 5 4, 5 5, 5 6
+         * 6 1, 6 2, 6 3, 6 4, 6 5, 6 6
+         *
+         *
+         *  pseudo code
+         *
+         *  class Dominio {
+         *      int val1;
+         *      int val2;
+         *
+         *  }
+         *
+         *  class Rules {
+         *      boolean Rule1()
+         *      boolean Rule2()
+         *      ... etc ...
+         *  }
+         *
+         *  class Game
+         *      list dominos in use
+         *      list dominos reserve
+         *      void buildDominioSet(); 1 1, 1 2, 1 3 ... 6 5, 6 6. == 36 unique dominos.
+         *      void calcPoints();
+         *  }
+         */
+
+    }
+}
