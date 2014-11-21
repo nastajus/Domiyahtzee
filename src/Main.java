60a6a5fd1aaafed338cbc42fb44a3796f1f97e52
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-21 11:39:40 -0500

    minor updates

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,19 @@ public class Main {
          *  15 16 17 18 19
          *  20 21 22 23 24
          *
-         * Set of all possible dominos == 6 x 6 == 36
+         * Set of all possible dominoes == 6 x 6 == 36
          *
-         * 1 1, 1 2, 1 3, 1 4, 1 5, 1 6
-         * 2 1, 2 2, 2 3, 2 4, 2 5, 2 6
-         * 3 1, 3 2, 3 3, 3 4, 3 5, 3 6
-         * 4 1, 4 2, 4 3, 4 4, 4 5, 4 6
-         * 5 1, 5 2, 5 3, 5 4, 5 5, 5 6
+         * 1 1
+         * 2 1, 2 2
+         * 3 1, 3 2, 3 3
+         * 4 1, 4 2, 4 3, 4 4
+         * 5 1, 5 2, 5 3, 5 4, 5 5
          * 6 1, 6 2, 6 3, 6 4, 6 5, 6 6
          *
          *
          *  pseudo code
          *
-         *  class Dominio {
+         *  class Domino {
          *      int val1;
          *      int val2;
          *
@@ -41,7 +41,7 @@ public class Main {
          *  class Game
          *      list dominos in use
          *      list dominos reserve
-         *      void buildDominioSet(); 1 1, 1 2, 1 3 ... 6 5, 6 6. == 36 unique dominos.
+         *      void buildDominioSet(); 1 1, ... 2 1, 2, 2, ... 6 5, 6 6. == 21 unique combinations.
          *      void calcPoints();
          *  }
          */
