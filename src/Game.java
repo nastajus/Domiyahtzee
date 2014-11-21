aeb996544eae407c3c5b45e7e7111afa8b9ebf93
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-21 12:18:45 -0500

    minor changes

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,34 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IAN on 21/11/14.
  */
 public class Game {
-    ArrayList<Domino> using;
-    ArrayList<Domino> reserve;
+
+    List<Domino> reserve = new ArrayList<Domino>();
+    List<Domino> using = new ArrayList<Domino>();
 
     void Create(){
 
         for(int i = Domino.creationDieSideCounter; i <= Domino.creationDieSideMax; i++ ){
             for( int j = 1; j <= i; j++){
                 Domino d = new Domino(Piece.DOMINO, Domino.creationDieSideCounter, j );
-                System.out.print ( d.toString() + ", ");
+                System.out.print(d.toString() + ", ");
+                reserve.add(d);
             }
             Domino.creationDieSideCounter++;
             System.out.println();
         }
 
         Domino d = new Domino( Piece.SINGLETON, 4, null);
-        System.out.println ( d.toString() );
+        System.out.println(d.toString());
+        reserve.add(d);
+    }
+
+    //later refactor to utilize input.txt.
+    void Populate(){
+        //For now hard-coding.
+        
     }
 }
